2a2e47d9963c5e52a80a8903139ca5d8afb14ca1
tanaka <ngn2349544@stu.o-hara.ac.jp>
2024-07-11 10:05:58 +0900

    一部修正しました。:宮澤

--- src/scoremanager/main/SubjectListAction.java
+++ src/scoremanager/main/SubjectListAction.java
@@ -7,46 +7,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.School;
 import bean.Subject;
 import bean.Teacher;
 import dao.SubjectDao;
 import tool.Action;
 
-@WebServlet(urlPatterns={"/subject/subject_list"})
+@WebServlet(urlPatterns={"/group-e/subject/subject_list"})
 public class SubjectListAction extends Action{
 
 	@Override
 	public void execute (HttpServletRequest req, HttpServletResponse res) throws Exception {
 		HttpSession session = req. getSession();
 		Teacher teacher = (Teacher)session.getAttribute("user");
+		School school = teacher.getSchool();
 
-		String name="";
-
+		Subject subject = new Subject();
 
 		SubjectDao subDao = new SubjectDao();
 
-		name = req.getParameter("f1");
+		List<Subject> list = subDao.filter(school);
 
-
-		//DBからデータ取得 3
-		// ログインユーザーの学校コードをもとに科目の一覧を取得
-
-		List<Subject> list = subDao.filter(teacher.getSchool());
-
-		req.setAttribute("f1", name);
+//		System.out.println(list);
+//		System.out.print("kuso");
 
 		req.setAttribute("subject_set", list);
-
-		req.setAttribute("name", name);
-
-		req.getRequestDispatcher("group-e/subject/subject_list.jsp").forward(req, res);
+	//DBからデータ取得 3
+		// ログインユーザーの学校コードをもとに科目の一覧を取得
 
 
-
-
-
-
-
+		req.getRequestDispatcher("main/subject_list.jsp").forward(req, res);
 
 		}
 
