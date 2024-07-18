f919e94d4b2a112f5a2a988f29d59b9dc7e8179d
djangouser <ngn2349603@stu.o-hara.ac.jp>
2024-07-18 09:58:08 +0900

    科目登録修正 松木

--- src/scoremanager/main/SubjectCreateAction.java
+++ src/scoremanager/main/SubjectCreateAction.java
@@ -5,23 +5,16 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import bean.Teacher;
 import tool.Action;
 
 public class SubjectCreateAction extends Action{
 
 	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-		HttpSession session = request. getSession();
-		Teacher teacher = (Teacher)session.getAttribute("user");
-//		System.out.print(teacher);
-
-		request.getRequestDispatcher("/scoremanager/main/subject_create.jsp").forward(request, response);
-
-
+//		JSPにフォワード
+		req.getRequestDispatcher("subject/subject_create.jsp").forward(req, res);
 
 	}
 }
