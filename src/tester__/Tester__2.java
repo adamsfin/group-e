3bbc2f417adac22001744db8ab11fc9dcdf4232d
djangouser <ngn2349535@stu.o-hara.ac.jp>
2024-07-24 10:08:43 +0900

    おはようめちゃくちゃお腹減った

--- src/tester__/Tester__2.java
+++ src/tester__/Tester__2.java
@@ -9,25 +9,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.Teacher;
-import dao.TeacherDao;
+import bean.Teacher; // beanパッケージからTeacherクラスをインポート
+import dao.TeacherDao; // daoパッケージからTeacherDaoクラスをインポート
 
 @WebServlet(urlPatterns={"/tester2"})
 public class Tester__2 extends HttpServlet {
 
-	public void doGet (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        try {
+            // セッションを取得する
+            HttpSession session = req.getSession();
 
-		try{
-//			StudentListAction listAction = new StudentListAction();
-			HttpSession session = req. getSession();
-			TeacherDao teacherDao = new TeacherDao();
-			Teacher teacher = teacherDao.get("admin");
-			session.setAttribute("user", teacher);
-//			listAction.execute(req, res);
-			req.getRequestDispatcher("tester.jsp").forward(req, res);;
-		} catch (Exception e) {
-			System.out.println("失敗");
-			e.printStackTrace();
-		}
-	}
+            // TeacherDaoクラスのインスタンスを生成する
+            TeacherDao teacherDao = new TeacherDao();
+
+            // "admin"というユーザー名の先生情報をデータベースから取得する
+            Teacher teacher = teacherDao.get("admin");
+
+            // セッションに取得した先生情報を"user"という名前で設定する
+            session.setAttribute("user", teacher);
+
+            // リクエストをtester.jspにフォワードする
+            req.getRequestDispatcher("tester.jsp").forward(req, res);
+        } catch (Exception e) {
+            System.out.println("失敗");
+            e.printStackTrace();
+        }
+    }
 }
